c75ac6a22e3475c03fab954ca55262600cbf8a62
gregoryreis <greis003@fiu.edu>
2026-02-05 14:36:55 -0500

    App 7 progress check 2

--- app7.py
+++ app7.py
@@ -13,3 +13,12 @@ st.title("Data Dashboard")
 st.sidebar.title("Load Data")
 uploadedFile = st.sidebar.file_uploader("Choose a CSV file",
                                         type=["csv"])
+
+if uploadedFile is not None:
+    df = pd.read_csv(uploadedFile) # Reading uploaded file from the user
+    st.sidebar.success("Data successfully loaded.")
+else:
+    st.sidebar.info("A default dataset will be used.")
+    df = pd.read_csv("biscayne_bay_water_quality2.csv") # default dataset
+
+tab1, tab2, tab3 = st.tabs(["Tables","Charts","Map"])
