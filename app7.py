4066fedb4a9e1a6c0e2cf6ced600a061246316b1
gregoryreis <greis003@fiu.edu>
2026-02-05 14:44:09 -0500

    App 7 progress check 3

--- app7.py
+++ app7.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd # data manipulation
 import plotly.express as px # data visualization
 
-# This code can be found at github.com/studentgreg/hci-spring2026
 
 st.set_page_config(layout="wide",
                    page_title="Data Dashboard",
@@ -22,3 +21,23 @@ else:
     df = pd.read_csv("biscayne_bay_water_quality2.csv") # default dataset
 
 tab1, tab2, tab3 = st.tabs(["Tables","Charts","Map"])
+
+with tab1:
+    st.header("Raw Data")
+    st.dataframe(df) # streamlit displays pandas dataframes
+
+    st.header("Summary Statistics")
+    st.dataframe(df.describe())
+
+with tab2:
+    st.header("Charts")
+    with st.expander("Line Chart"):
+        parameter_selected = st.selectbox(
+            "Select a parameter:",
+            df.columns
+        )
+        fig1 = px.line(df,
+                       x="Time",
+                       y=parameter_selected,
+                       title=f"{parameter_selected} over Time")
+        st.plotly_chart(fig1) # streamlit displays plotly charts
