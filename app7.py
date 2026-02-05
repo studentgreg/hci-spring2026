ae67a243d2c8a47800d355015538850220f44705
gregoryreis <greis003@fiu.edu>
2026-02-05 14:32:07 -0500

    App 7 progress check 1

--- app7.py
+++ app7.py
@@ -1,2 +1,15 @@
 import streamlit as st
-import pandas as pd # data manipulation 
+import pandas as pd # data manipulation
+import plotly.express as px # data visualization
+
+# This code can be found at github.com/studentgreg/hci-spring2026
+
+st.set_page_config(layout="wide",
+                   page_title="Data Dashboard",
+                   page_icon=":bar_chart:")
+
+st.title("Data Dashboard")
+
+st.sidebar.title("Load Data")
+uploadedFile = st.sidebar.file_uploader("Choose a CSV file",
+                                        type=["csv"])
